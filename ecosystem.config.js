1cb3234bf856147975653895e44b261dfe8ef414
andvargas <a.vargyas@gmail.com>
2021-02-19 17:22:10 +0000

    Setup PM2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
         production: {
             user: 'ubuntu',
             host: 'ec2-3-11-204-178.eu-west-2.compute.amazonaws.com',
-            key: '~/.ssh/id_rsa.pub',
+            key: '~/.ssh/andras-byroom.pem',
             ref: 'origin/main',
             repo: 'git@github.com:andvargas/api.andrasvargas.git',
             path: '/home/ubuntu/api.andrasvargas',
