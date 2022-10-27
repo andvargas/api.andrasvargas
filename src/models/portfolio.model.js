5f7dce8e9086c59b314cee25c0fbce9f8acda341
andvargas <a.vargyas@gmail.com>
2022-10-27 18:03:39 +0100

    added status to projects

--- src/models/portfolio.model.js
+++ src/models/portfolio.model.js
@@ -1,23 +1,27 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const portfolioSchema = new Schema({
+const portfolioSchema = new Schema(
+  {
     title: {
-        type: String,
-        required: true,
-        trim: true,
-        minlength: 3
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 3,
     },
     description: { type: String, required: true },
     image: { type: Buffer },
     link: { type: String },
+    status: { type: String },
     buttonLeft: { type: String },
-    buttonRight: { type: String }
-}, {
+    buttonRight: { type: String },
+  },
+  {
     timestamps: true,
-});
+  }
+);
 
-const Project = mongoose.model('Project', portfolioSchema);
+const Project = mongoose.model("Project", portfolioSchema);
 
 module.exports = Project;
