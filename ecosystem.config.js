a671e9502f0cba75374241cc78e929f026b27881
andvargas <a.vargyas@gmail.com>
2023-01-16 11:42:23 +0000

    implemented pm2 deploy

---
+++ ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  apps: [
+    {
+      name: "api",
+      script: "./index.js",
+      restart_delay: 4000,
+      out_file: "/var/log/pm2/api/out.log",
+      error_file: "/var/log/pm2/api/out.log",
+    },
+  ],
+  deploy: {
+    production: {
+      user: "andras",
+      host: ["31.24.230.163"],
+      ref: "origin/main",
+      repo: "https://github.com/andvargas/api.andrasvargas.git",
+      path: "/var/www/api",
+      "post-setup": "npm install",
+      "post-deploy": "pm2 startOrRestart ecosystem.config.js --env production",
+    },
+  },
+};
